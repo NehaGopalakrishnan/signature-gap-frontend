4419106e967e3a826177949e036b63366cb82dac
NehaGopalakrishnan <morana8373@gmail.com>
2026-03-24 18:04:42 +0530

    Update Mask.jsx

--- src/pages/Mask.jsx
+++ src/pages/Mask.jsx
@@ -1,12 +1,1 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
-export default function Mask() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate("/processing");
-  }, [navigate]);
-
-  return null;
-}
